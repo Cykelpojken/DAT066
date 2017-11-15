c8b095d404c8c58aee9a31c8846caeabaa25a4c5
Cykelpojken <nislga@student.chalmers.se>
2017-11-15 15:39:27 +0100

    Fungerar med en lista av gameobjects, förutsatt att listan inte innehåller object som är barn 2 generationer ner från main objectet

--- Test3d/Assets/3d/Scripts/Change_shaders.cs
+++ Test3d/Assets/3d/Scripts/Change_shaders.cs
@@ -1,38 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Change_shaders : MonoBehaviour
 {
-
-    public void Do_shit(GameObject e)
+    public void Click()
     {
-        //Color black = new Color(0, 0, 0, 1);
-        /*Material[] Materials;
-        Materials = GetComponents<Material>();*/
+        GameObject g = GameObject.Find("chassis01");
+        GameObject p = GameObject.Find("front_ty23");
+        GameObject[] e = { g, p };
+        Highlight(e);
+    }
 
-        List<GameObject> GameObjectList = new List<GameObject>();
-        Material[] MaterialList;
-        MeshRenderer Meshrenderer;
+    Material[] MaterialList;
+    MeshRenderer Meshrenderer;
+    public void Highlight(GameObject[] e) //Works provided that all Gameobjects are not decendent more then two childs down from main object
+    {
+        Transform[] ob = GameObject.Find("scania").GetComponentsInChildren<Transform>(); //Gets children by parent name 
+        List<GameObject> RemoveList = new List<GameObject>();
 
-        Transform[] ob = GameObject.Find("scania").GetComponentsInChildren<Transform>(); //Gets children by parent name
-        foreach (Transform t in ob) 
-        {
-            if (t.gameObject.name != "scania" && t.gameObject.name != e.transform.parent.name) //Does not hide parent
-                t.gameObject.SetActive(false);
-
-            GameObjectList.Add(t.gameObject);
-            if (t.gameObject.name == e.name)//Does not hide the argument-object
+        foreach (GameObject g in e) {
+            for(int i = 0; i < ob.Length; i++)
             {
-                t.gameObject.SetActive(true);
-
-                Meshrenderer = t.gameObject.GetComponent(typeof(MeshRenderer)) as MeshRenderer; //Works provided that all gameobjects uses MeshRenderer
-                MaterialList = Meshrenderer.materials;
-                foreach (Material m in MaterialList)
+                
+                if (ob[i].name == g.transform.name)
                 {
-                    m.SetColor("_Color", new Color(1, 1, 0));
+                    GameObject temp = g;
+                    bool parentexist = true;
+                    while(parentexist)
+                    {
+                        
+                        if(!RemoveList.Contains(temp)) //Adds all the objects that are to be ignored to a list
+                            RemoveList.Add(temp);
+
+                        if (temp.transform.parent == null)
+                            parentexist = false;
+                        else
+                            temp = temp.transform.parent.gameObject;
+                    }
                 }
             }
+            for(int i = 0; i < ob.Length; i++)
+            {
+                if (RemoveList.Contains(ob[i].gameObject))
+                {
+                    var foos = new List<Transform>(ob); //Removes the gameobjects from ob
+                    foos.RemoveAt(i);
+                    ob = foos.ToArray();
+                }
+            }
+        }
+        foreach (GameObject g in e)
+        {
+            HideAndHighlight(ob, g); //Sends in the list that no longer contains the choosen Gameobjects, and one of the game objects that is to be hidden
+        }
+        
+    }
+
+    public void HideAndHighlight(Transform[] ob, GameObject e)
+    {
+        foreach(Transform t in ob)
+        {
+            if (t.gameObject.name != e.transform.parent.name)
+            {
+                t.gameObject.SetActive(false);
+            }
+        }
+        e.SetActive(true);
+        Meshrenderer = e.GetComponent(typeof(MeshRenderer)) as MeshRenderer; //Works provided that all gameobjects uses MeshRenderer
+        MaterialList = Meshrenderer.materials;
+        foreach (Material m in MaterialList)
+        {
+            m.SetColor("_Color", new Color(1, 1, 0));
         }
     }
 }
